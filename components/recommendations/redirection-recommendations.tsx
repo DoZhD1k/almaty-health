3066cf6ff53e9398867a73c0cde84f848e5973b7
Arlen Chereshnikov <chereshnikovarlen03@gmail.com>
2025-11-28 17:05:32 +0500

    max overload changed from 0.7 to 0.85

--- components/recommendations/redirection-recommendations.tsx
+++ components/recommendations/redirection-recommendations.tsx
@@ -64,7 +64,7 @@ export function RedirectionRecommendations({
 
         // Находим перегруженные МО (> 70%)
         const overloaded = allFacilities
-          .filter((f) => f.occupancy_rate_percent > 0.7)
+          .filter((f) => f.occupancy_rate_percent > 0.85)
           .sort((a, b) => b.occupancy_rate_percent - a.occupancy_rate_percent);
 
         // Для каждого перегруженного МО находим альтернативы
