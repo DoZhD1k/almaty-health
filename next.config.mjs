648bb24403b50ba1600d303f9d178a468a8841db
ulpashk <ukuanishbek@icloud.com>
2026-06-29 14:32:39 +0500

    small fix for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isVercel = process.env.VERCEL === '1';
+
 const nextConfig = {
   output: 'standalone',
-  basePath: '/hospital-admissions',
+  // basePath: '/hospital-admissions',
+  basePath: isVercel ? '' : '/hospital-admissions',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
